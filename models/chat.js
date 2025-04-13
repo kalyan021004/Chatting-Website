64efe32d0ab7ee92b6efc1a1d307c58c4a494bb9
vk <kalyan021004@gmail.com>
2025-04-13 15:24:54 +0530

    initial

--- models/chat.js
+++ models/chat.js
@@ -1,24 +1,13 @@
-const mongoose=require("mongoose");
+const mongoose = require('mongoose');
 
-const chatSchema=new mongoose.Schema({
-    from:{
-        type:String,
-        required:true
-    },
-    to:{
-        type:String,
-        required:true
-    },
-    msg:{
-        type:String,
-        maxLength:50
-    },
-    created_at:{
-        type:Date,
-        required:true
-    },
+const chatSchema = new mongoose.Schema({
+  from: String,
+  to: String,
+  msg: String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Chat =mongoose.model("Chat",chatSchema);
-
-module.exports=Chat;
+module.exports = mongoose.model('Chat', chatSchema);
